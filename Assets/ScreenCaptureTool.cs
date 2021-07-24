679df9886ef3bc42dbbb851ae35d317197f72ba8
Bronson Zgeb <bronson.zgeb@unity3d.com>
2021-07-23 22:26:09 -0400

    example screenshot

---
+++ Assets/ScreenCaptureTool.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class ScreenCaptureTool : MonoBehaviour
+{
+    [SerializeField] string filename;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            ScreenCapture.CaptureScreenshot($"{filename}.png");
+        }
+    }
+}
